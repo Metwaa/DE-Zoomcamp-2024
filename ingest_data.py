637d2838af74d0459161452a433674e8f46baf6f
Metwaa <156903139+Metwaa@users.noreply.github.com>
2024-01-30 17:26:59 +0000

    test ingestion

--- ingest_data.py
+++ ingest_data.py
@@ -23,16 +23,16 @@ def main(params):
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}');
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
     df = next(df_iter)
-    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
-    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    #df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    #df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
     while True: 
         t_start = time()
         df = next(df_iter)
-        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
-        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    #    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    #    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
         df.to_sql(name=table_name, con=engine, if_exists='append')
         t_end = time()
         print('inserted another chunk, took %.3f second' % (t_end - t_start))
