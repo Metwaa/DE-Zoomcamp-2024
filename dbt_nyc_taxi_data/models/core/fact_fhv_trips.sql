9b9d4df7d854448746fceaf3d534719e5571584b
Amr Metwally <a.metwaa@gmail.com>
2024-02-24 08:48:24 +0000

    fact table for fhv trips

---
+++ dbt_nyc_taxi_data/models/core/fact_fhv_trips.sql
@@ -0,0 +1,24 @@
+{{ config(materialized="table") }}
+
+
+with pickup_zone as (
+        select locationid, borough as pickup_borough, zone as pickup_zone
+        from {{ ref("dim_zones") }}
+        where borough != 'Unknown'
+),
+dropoff_zone as (
+    select locationid, borough as dropoff_borough, zone as dropoff_zone
+    from {{ ref("dim_zones") }}
+    where borough != 'Unknown'
+)
+select
+    dispatching_base_num,
+    pickup_zone.pickup_borough,
+    pickup_zone.pickup_zone,
+    dropoff_zone.dropoff_borough,
+    dropoff_zone.dropoff_zone,
+    pickup_datetime,
+    dropoff_datetime,
+from {{ ref("stg_fhv_tripdata") }}
+inner join pickup_zone on stg_fhv_tripdata.PUlocationID= pickup_zone.locationid
+inner join dropoff_zone on stg_fhv_tripdata.DOlocationID = dropoff_zone.locationid
